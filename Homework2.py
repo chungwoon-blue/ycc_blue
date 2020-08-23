44dbe46a5eecbba3444125f1c8e3ce08d60e20e3
chungwoon-blue <x21919@naver.cm>
2020-08-23 14:17:05 +0900

    done

---
+++ Homework2.py
@@ -0,0 +1,15 @@
+price_sm = 115500
+eps_sm = 1685.38
+marketvalue_sm = 21585500000000
+bookvalue_sm = 23720329670300
+
+per_sm = price_sm / eps_sm
+pbr_sm = marketvalue_sm / bookvalue_sm
+
+print(per_sm)
+print(pbr_sm)
+
+comment = f"삼성물산의 PER은 {per_sm}이며 PBR은 {pbr_sm}입니다"
+
+print(comment)
+
